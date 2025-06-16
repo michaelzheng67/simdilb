d1e8739d26e2cc7e65d60b4cea0823034809fde5
michaelzheng67 <michaelzheng67@yahoo.ca>
2025-06-16 13:12:18 -0400

    more tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,7 @@
 import pytest
-from simdlib import (sum_list, multiply_list, min_list, max_list, any_list, all_list
+from simdlib import (sum_list, multiply_list, min_list, max_list, any_list, all_list,
+                     add_each_list, subtract_each_list, multiply_each_list, square_each_list,
+                     subtract_and_square_each_list
                      )
 
 def test_sum():
@@ -52,3 +54,33 @@ def test_all():
         all_list([])
     with pytest.raises(SystemError):
         all_list(["hello", "world"])
+
+def test_add_each():
+    assert add_each_list([1, 2, 3], 1) == [2, 3, 4]
+    assert add_each_list([0, 0, 0], 0) == [0, 0, 0]
+    with pytest.raises(SystemError):
+        add_each_list([], 1)
+
+def test_subtract_each():
+    assert subtract_each_list([1.0, 2.0, 3.0], 1.0) == [0.0, 1.0, 2.0]
+    assert subtract_each_list([0.0, 0.0, 0.0], 0) == [0.0, 0.0, 0.0]
+    with pytest.raises(SystemError):
+        subtract_each_list([], 0)
+
+def test_multiply_each():
+    assert multiply_each_list([1, 2, 3], 2) == [2, 4, 6]
+    assert multiply_each_list([1, 2, 3], 1) == [1, 2, 3]
+    with pytest.raises(SystemError):
+        multiply_each_list([], 0)
+
+def test_square_each():
+    assert square_each_list([1.0, 2.0, 3.0]) == [1.0, 4.0, 9.0]
+    assert square_each_list([0.0, 0.0, 0.0]) == [0.0, 0.0, 0.0]
+    with pytest.raises(SystemError):
+        square_each_list([])
+
+def test_subtract_and_square_each():
+    assert subtract_and_square_each_list([1.0, 2.0, 3.0], 1) == [0.0, 1.0, 4.0]
+    assert subtract_and_square_each_list([1.0, 2.0, 3.0], 0) == [1.0, 4.0, 9.0]
+    with pytest.raises(SystemError):
+        subtract_and_square_each_list([], 0)
